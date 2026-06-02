263102d31ae3661b5be04562b1754f8e78ed89e9
Ujjwalg2104 <garudujjwal@gmail.com>
2026-06-02 13:41:15 +0530

    Refactor first_three_sapce_print_starpattern_invert class to improve main method structure and readability

--- DAY_22/first_three_sapce_print_starpattern_invert.java
+++ DAY_22/first_three_sapce_print_starpattern_invert.java
@@ -1,5 +1,20 @@
 package DAY_22;
 
 public class first_three_sapce_print_starpattern_invert {
-    
+    public static void main(String args[]) {
+        int line = 4;
+
+        for(int row = 1; row <= line; row++) {
+
+            for(int space = 1; space < row; space++) {
+                System.out.print(" ");
+            }
+
+            for(int star = 1; star <= line - row + 1; star++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
 }
