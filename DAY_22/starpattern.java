0438c93154bbdeb359856625a13d8667df04a317
Ujjwalg2104 <garudujjwal@gmail.com>
2026-06-02 11:36:06 +0530

    Update starpattern class to increase the number of rows printed from 4 to 6

--- DAY_22/starpattern.java
+++ DAY_22/starpattern.java
@@ -3,7 +3,7 @@ package DAY_22;
 public class starpattern {
      public static void main(String args[]) {
 
-        for(int i = 1; i <= 4; i++) {
+        for(int i = 1; i <= 6; i++) {
             for(int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
