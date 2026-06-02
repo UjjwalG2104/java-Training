90a621bce455e6d63b22a8ca35288d792f52ad2b
Ujjwalg2104 <garudujjwal@gmail.com>
2026-06-02 11:08:30 +0530

    Add Forloop and table_of_2 classes for basic Java examples

---
+++ DAY_22/Forloop.java
@@ -0,0 +1,14 @@
+package DAY_22;
+
+public class Forloop {
+    public static void main(String[] args) {
+        
+        for (int chinnki =  1; chinnki <= 20; chinnki++) {
+            if (chinnki % 2 == 0){
+                System.out.println(chinnki);
+             }
+        }   
+    }
+
+}
+            
