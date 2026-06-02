7e11b8c4db121edd1032deec21100a46bbfd3de8
Ujjwalg2104 <garudujjwal@gmail.com>
2026-06-02 11:09:52 +0530

    Refactor table_of_2 class to simplify output format in main method

--- DAY_22/table_of_2.java
+++ DAY_22/table_of_2.java
@@ -1,13 +1,10 @@
 package DAY_22;
 
 public class table_of_2 {
-    public static void main(String[] args) {
-        int num = 2;
-
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(num + " x " + i + " = " + (num * i));
+    public static void main(String args[]) {
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(2 * i);
         }
     }
-}
-    
+} 
 
