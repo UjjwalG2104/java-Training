1710cfe5255815484d9ce46c05c7f2c173274070
Ujjwalg2104 <garudujjwal@gmail.com>
2026-06-02 11:51:26 +0530

    Add starprintinver class to print inverted star pattern

---
+++ DAY_22/starprintinver.java
@@ -0,0 +1,15 @@
+package DAY_22;
+
+public class starprintinver {
+     public static void main(String args[]) {
+
+        for(int row = 4; row >= 1; row--) {
+            for(int col = 1; col <= row; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
+    
